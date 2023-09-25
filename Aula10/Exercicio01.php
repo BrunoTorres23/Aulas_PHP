be6146d08c6d82138e22e86994bf7fd054818108
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2023-09-25 13:25:43 -0300

    Não finalizado
    Finalizar prática e conceito de switch/case

---
+++ Aula10/Exercicio01.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+
+<body>
+  <div>
+    <?php
+    $n = isset($_GET["num"]) ? $_GET["num"] : 0;
+    $o = isset($_GET["oper"]) ? $_GET["oper"] : 1;
+    switch ($o) {
+      case 1:
+        $r = $n * 2;
+        break;
+      case 2:
+        $r = $n ^ 3;
+        break;
+      case 3:
+        $r = sqrt($n);
+    }
+    // aqui são casos de escolhas, o usuario irá escolher uma das opçoes, e cada case irá trazer
+    // uma operação diferente.
+
+    echo "O resultado da operação solicitada foi: <span class='foco'>$r</span>"
+    ?>
+  </div>
+</body>
+
+</html>
---
+++ Aula10/Exercicio02.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+
+<body>
+  <div>
+    <?php
+      $d = isset($_GET["ds"])?$_GET["ds"]:0;
+      switch ($d) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 2:
+        case 2:
+      }
+    ?>
+    <br><a href="javascript:history.go(-1)" class="botao">Voltar</a>
+    <!-- este botão em js significa voltar para a pagina anterior -->
+  </div>
+</body>
+
+</html>
