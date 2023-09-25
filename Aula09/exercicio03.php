c3a02aa105c62dc0c6be4dc4a47871c2953b7bc9
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2023-09-25 09:53:56 -0300

    Finalizado
    Exercicio de media do aluno realizado.

--- Aula09/exercicio03.php
+++ Aula09/exercicio03.php
@@ -10,8 +10,24 @@
 <body>
   <div>
     <?php
+    $nota1 = isset($_GET["n1"]) ? $_GET["n1"] : "Não foi possivel ler a sua nota.";
+    $nota2 = isset($_GET["n2"]) ? $_GET["n2"] : "Não foi possivel ler a sua nota.";
 
+    $media = ($nota1 + $nota2) / 2;
+
+    if ($media >= 7 || $media == 6) {
+      $sit = "APROVADO";
+    } elseif ($media < 6) {
+      $sit = "REPROVADO";
+    }
+    
+
+    echo "A sua primeira nota foi: $nota1<br>
+            A sua segunda nota foi: $nota2<br>
+            Logo, a sua media final foi: $media<br>
+            Situação final do aluno: $sit";
     ?>
+
   </div>
 </body>
 
