ee2597047fdd67c7bca71bc27358b7aa55a97b46
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2023-09-26 14:01:49 -0300

    Não concluido

--- Aula10/Exercicio02.php
+++ Aula10/Exercicio02.php
@@ -10,15 +10,23 @@
 <body>
   <div>
     <?php
-      $d = isset($_GET["ds"])?$_GET["ds"]:0;
-      switch ($d) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 2:
-        case 2:
-      }
+    $d = isset($_GET["ds"]) ? $_GET["ds"] : 0;
+    switch ($d) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        echo "Dia letivo.";
+        break;
+      case 7:
+      case 8:
+        echo "Dia não letivo";
+        break;
+      default:
+        echo "Dia inválido.";
+    }
+
     ?>
     <br><a href="javascript:history.go(-1)" class="botao">Voltar</a>
     <!-- este botão em js significa voltar para a pagina anterior -->
