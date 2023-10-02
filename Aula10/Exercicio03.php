1349615fcf69e041baf4001955d586e05d338019
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2023-10-02 12:50:25 -0300

    Finalizado
    Não estão todos os estados e regiões, mas a lógica e o código estão corretos.

--- Aula10/Exercicio03.php
+++ Aula10/Exercicio03.php
@@ -10,7 +10,28 @@
 <body>
   <div>
     <?php
-
+    $estado = isset($_GET["est"]) ? $_GET["est"] : 0;
+    switch ($estado) {
+      case 'sp':
+      case 'rj':
+      case 'es':
+      case 'mg':
+        echo "Você escolheu a região sudeste";
+        break;
+      case 'pr':
+      case 'sc':
+      case 'rs':
+        echo "Você escolheu a região sul";
+        break;
+      case 'am';
+      case 'pa';
+      case 'ac';
+      case 'rr';
+      case 'ro';
+      case 'ap';
+      case 'to';
+        echo "Você escolheu a região norte";
+    }
     ?>
   </div>
 </body>
